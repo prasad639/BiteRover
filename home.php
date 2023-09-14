48c7f1b80e4fe99594c9f1711162b98206b6acd0
Prasad <121895548+prasad639@users.noreply.github.com>
2023-09-15 00:55:47 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,49 @@
+
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<title>Zomato</title>
+<link rel="stylesheet" href="home.css">
+<link rel="icon" href="icon.png">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css
+" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+    <header>
+
+        <ul class="nav">
+            <li> <a href="home.php">home</a></li>
+            <li> <a href="http://localhost/php/zomato/add_restaurant.php">Add your restaurant</a></li>
+            <li> <a href="login.php">login</a> </li>
+            <li> <a href="sign_up.html">sign up</a> </li>
+            <li> <a href="http://localhost/php/zomato/show_restaurant.php">See Restraurant</a> </li>
+        </ul>
+        <h1 id="h1">Zomato</h1>
+        <br><br>
+        <h3 id="h2">find the best restaurant,bars and cafes in india</h3>
+
+    </header>
+
+    <section>
+        <div class="header-img">
+            <img id="img"src="zomato_home_bgrnd.jpg">
+        </div>
+
+        
+    </section>
+
+
+   
+
+     
+</body>
+
+</html>
